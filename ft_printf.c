682826abc3f3efc206f87890d40b6d5044843a5a
omaalii <omarmoustafa0024@gmail.com>
2023-11-06 22:20:14 +0100

    second

--- ft_printf.c
+++ ft_printf.c
@@ -6,26 +6,31 @@
 /*   By: omaali <omaali@student.42barcelon>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 03:24:14 by omaali            #+#    #+#             */
-/*   Updated: 2023/10/29 08:23:42 by omaali           ###   ########.fr       */
+/*   Updated: 2023/11/06 22:19:39 by omaali           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <stdio.h>
 
-int	ft_printf(const format*, ...)
-{
-	va_list	arg;
-	int		count;
+int ft_printf(const str*, ...)
+ {
+     va_list arg;
+     int     count;
 
-	va_start(arg, format);
-	count = 0;
-	while (*format != '\0')
-	{
-		if (*format == '%')
-			print_format(*(format++))
-		else
-			ft_putchr(chr, count)
-
+     va_start(arg, str);
+     count = 0;
+     while (*str != '\0')
+     {
+         if (*str == '%')
+             print_format(*(str++, arg));
+         else
+             ft_putchr(chr, count);
+         count++;
+         str++;
+     }
+     va_end(arg);
+     return count;
+ }
 
 
